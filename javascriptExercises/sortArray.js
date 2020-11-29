9d17ef623219f5feb5c4c01b387f8a0c8b9e8f86
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-11-28 22:15:27 -0500

    saving sort exercise

---
+++ javascriptExercises/sortArray.js
@@ -0,0 +1,9 @@
+//Sorting an Array
+var clients = ["Carlos", "Juan", "Pablo", "Lucas", "Zaray", "Abel"];
+
+function defaultVal(array) {
+    array.sort();
+    return array;
+};
+
+defaultVal(clients);
